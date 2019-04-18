4074a5b1fb6c0c1aa7d9ae35de932281ddf1da6f
Barney Hsiao <barney.hsiao@ADOPTPASTSAND.workdayinternal.com>
2019-04-18 01:29:04 -0700

    Updates

---
+++ precache-manifest.d925832770df23f03513a0a9b33bc551.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7eb3976a94f24dd02b86",
+    "url": "/react-github-repo-commit-view/static/js/runtime~main.ff83aa24.js"
+  },
+  {
+    "revision": "69ad9ab18ad08f69307f",
+    "url": "/react-github-repo-commit-view/static/js/main.181bb894.chunk.js"
+  },
+  {
+    "revision": "a7c6777ddce51658ed04",
+    "url": "/react-github-repo-commit-view/static/js/2.c638cc65.chunk.js"
+  },
+  {
+    "revision": "69ad9ab18ad08f69307f",
+    "url": "/react-github-repo-commit-view/static/css/main.3b2caaef.chunk.css"
+  },
+  {
+    "revision": "5ec3226e9f8418fd9ffc838893a73cdb",
+    "url": "/react-github-repo-commit-view/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-tutorial/precache-manifest.4bdd5e1b162198e42e5481423fdc7e6d.js"
+  "/react-github-repo-commit-view/precache-manifest.d925832770df23f03513a0a9b33bc551.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-tutorial/index.html", {
+workbox.routing.registerNavigationRoute("/react-github-repo-commit-view/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
